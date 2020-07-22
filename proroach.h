625432f3272ad6c9312d006ce2a61b6a7cafedae
matheustomieiro <matheustomieiro@usp.br>
2020-07-21 22:45:20 -0300

    Adicionando definicoes e passando a funcao print para a proroach

--- proroach.h
+++ proroach.h
@@ -4,8 +4,6 @@
 #include <math.h>
 #include <ctype.h>
 
-void printVector(char type, void *vet, int elem);
-
 /*Definindo estruturas de abertura e fechamento*/
 #define INICIO {
 #define FIM }
@@ -14,20 +12,29 @@ void printVector(char type, void *vet, int elem);
 #define INTEIRO int
 #define REAL double
 #define CARACTER char
+#define VAZIO void
 #define ENDERECO_DE &
 #define COMO_PONTEIRO *
-//Para palavras
+#define PONTEIRO *
+
+//Para vetores
+#define COMO_VETOR_DE [
 #define PALAVRA char
 #define DE [
+#define LETRAS ]
 #define ELEMENTOS ]
 #define INICIO_CONJUNTO {
-#define FINAL_CONJUNTO }
+#define FIM_CONJUNTO }
+
+/*Definindo simbolos diversos*/
+#define E_TAMBEM ,
+#define ALEM_DISSO ;
+#define FIM_DE_COMANDO ;
 
 /*Definindo funcao main*/
-#define INICIAR int
-#define PROGRAMA_MI main(void){
+#define COMECAR int
+#define PROGRAMA main(void)
 #define FINALIZAR return
-#define PROGRAMA_MF }
 #define SEM_ERRO 0;
 #define COM_ERRO 1;
 
@@ -39,7 +46,12 @@ void printVector(char type, void *vet, int elem);
 /*Definindo estruturas de controle*/
 #define ENQUANTO while(
 #define PARA for(
+#define COMECANDO_EM =
 #define FAZER ){
+
+/*Definindo acesso*/
+#define NA_POSICAO [
+#define DO_VETOR ]
 
 /*Definindo alocacao de tipos*/
 #define ALOCAR malloc(
@@ -62,14 +74,21 @@ void printVector(char type, void *vet, int elem);
 
 #define AUTO_SOMA +=
 #define AUTO_SUBTRAI -=
-
+#define INCREMENTANDO ++
 
 /*Definindo comparadores*/
-#define IGUAL ==
-#define MAIOR >
-#define MENOR <
-#define MENOR_IGUAL <=
-#define MAIOR_IGUAL >=
+#define IGUAL_A ==
+#define MAIOR_QUE >
+#define MENOR_QUE <
+#define MENOR_IGUAL_A <=
+#define MAIOR_IGUAL_A >=
+
+/*Definindo funcoes*/
+#define FUNCAO
+#define COM (
+#define COMO_ARGUMENTOS )
+
+VAZIO printVector COM CARACTER type E_TAMBEM VAZIO COMO_PONTEIRO vet E_TAMBEM INTEIRO elem COMO_ARGUMENTOS;
 
 /*Definindo funcoes de complexidade baixa*/
 #define EXIBIR_VETOR printVector(
@@ -77,16 +96,16 @@ void printVector(char type, void *vet, int elem);
 #define DE_REAIS 'r',
 #define DE_CHARACTERES 'c',
 #define DE_PONTEIROS 'p',
-#define COM ,
 #define ELEMENTOS_NA_TELA )
-#define EXIBIR_LINHA printf("\n")
+#define QUEBRA_DE_LINHA printf("\n")
+#define CONTENDO ,
 
 /*Definindo funcoes de complexidade media*/
-#define EXIBIR_INTEIRO printf("%d\n",
-#define EXIBIR_REAL printf("%lf\n",
-#define EXIBIR_CARACTER printf("%c\n",
-#define EXIBIR_PALAVRA printf("%s\n",
-#define EXIBIR_PONTEIRO printf("%p\n",
+#define EXIBIR_INTEIRO printf("%d ",
+#define EXIBIR_REAL printf("%lf ",
+#define EXIBIR_CARACTER printf("%c ",
+#define EXIBIR_PALAVRA printf("%s ",
+#define EXIBIR_PONTEIRO printf("%p ",
 #define NA_TELA )
 
 /*Definindo chamada de funcoes de complexidade alta*/
@@ -95,31 +114,34 @@ void printVector(char type, void *vet, int elem);
 
 /*Funcoes utilizadas*/
 
-void printVector(char type, void *vet, int elem){
-    char mascara[6];
-    if(type == 'i'){
-        for(int i=0; i<elem; i++){
-            printf("%d ",((int*)vet)[i]);
-        }
-        EXIBIR_LINHA;
-    }
-    else if(type == 'r'){
-        for(int i=0; i<elem; i++){
-            printf("%lf ",((double*)vet)[i]);
-        }
-        EXIBIR_LINHA;
-    }
-    else if(type == 'c'){
-        for(int i=0; i<elem; i++){
-            printf("%c ",((char*)vet)[i]);
-        }
-        EXIBIR_LINHA;
-    }
-    else if(type == 'p'){
-        for(int i=0; i<elem; i++){
-            printf("%p ",((int**)vet)[i]);
-        }
-        EXIBIR_LINHA;
-    }
-
-}
+VAZIO FUNCAO printVector COM CARACTER type E_TAMBEM VAZIO COMO_PONTEIRO vet E_TAMBEM INTEIRO elem COMO_ARGUMENTOS INICIO
+    CARACTER mascara COMO_VETOR_DE 6 ELEMENTOS FIM_DE_COMANDO
+    SE type IGUAL_A 'i' ENTAO
+       PARA INTEIRO i COMECANDO_EM 0 ALEM_DISSO i MENOR_QUE elem ALEM_DISSO i INCREMENTANDO FAZER
+            EXIBIR_INTEIRO ((INTEIRO COMO_PONTEIRO)vet) NA_POSICAO i DO_VETOR NA_TELA FIM_DE_COMANDO
+        FIM
+        QUEBRA_DE_LINHA FIM_DE_COMANDO
+        FINALIZAR FIM_DE_COMANDO
+    FIM
+    SE type IGUAL_A 'r' ENTAO
+        PARA INTEIRO i COMECANDO_EM 0 ALEM_DISSO i MENOR_QUE elem ALEM_DISSO i INCREMENTANDO FAZER
+           EXIBIR_REAL ((REAL COMO_PONTEIRO)vet) NA_POSICAO i DO_VETOR NA_TELA FIM_DE_COMANDO
+        FIM
+        QUEBRA_DE_LINHA FIM_DE_COMANDO   
+        FINALIZAR FIM_DE_COMANDO
+    FIM
+    SE type IGUAL_A 'c' ENTAO
+        PARA INTEIRO i COMECANDO_EM 0 ALEM_DISSO i MENOR_QUE elem ALEM_DISSO i INCREMENTANDO FAZER
+            EXIBIR_CARACTER ((CARACTER COMO_PONTEIRO)vet) NA_POSICAO i DO_VETOR NA_TELA FIM_DE_COMANDO
+        FIM
+        QUEBRA_DE_LINHA FIM_DE_COMANDO
+        FINALIZAR FIM_DE_COMANDO
+    FIM
+    SE type IGUAL_A 'p' ENTAO
+        PARA INTEIRO i COMECANDO_EM 0 ALEM_DISSO i MENOR_QUE elem ALEM_DISSO i INCREMENTANDO FAZER
+           EXIBIR_PONTEIRO ((INTEIRO COMO_PONTEIRO COMO_PONTEIRO)vet) NA_POSICAO i DO_VETOR NA_TELA FIM_DE_COMANDO
+        FIM
+        QUEBRA_DE_LINHA FIM_DE_COMANDO
+        FINALIZAR FIM_DE_COMANDO
+    FIM
+FIM
